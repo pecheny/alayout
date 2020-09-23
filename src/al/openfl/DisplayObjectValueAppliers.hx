4a5094426bf98ab3e52d200a027b6d32ed335ef4
artem pecheny <pecheny@gmail.com>
2020-09-23 16:02:53 +0300

    Update property setters for x, y, width, etc.

--- src/al/openfl/DisplayObjectValueAppliers.hx
+++ src/al/openfl/DisplayObjectValueAppliers.hx
@@ -1,16 +1,11 @@
 package al.openfl;
 import al.appliers.PropertyAccessors.PropertyAccessor;
 import al.appliers.PropertyAccessors.FloatPropertyAccessor;
-import openfl.display.DisplayObject;
-class DisplayObjectValueAppliers {
-    public function new() {
-    }
-}
 
-@:property("scaleX") class DOScaleXPropertySetter<T:DisplayObject> implements PropertyAccessor<Float> implements FloatPropertyAccessor {}
-@:property("scaleY") class DOScaleYPropertySetter<T:DisplayObject> implements PropertyAccessor<Float> implements FloatPropertyAccessor {}
+@:property("scaleX") class DOScaleXPropertySetter<T:{scaleX:Float}> implements PropertyAccessor<Float> implements FloatPropertyAccessor {}
+@:property("scaleY") class DOScaleYPropertySetter<T:{scaleY:Float}> implements PropertyAccessor<Float> implements FloatPropertyAccessor {}
 @:property("width") class DOWidthPropertySetter<T:DisplayObject> implements PropertyAccessor<Float> implements FloatPropertyAccessor {}
 @:property("height") class DOHeightPropertySetter<T:DisplayObject> implements PropertyAccessor<Float> implements FloatPropertyAccessor {}
 @:property("x") class DOXPropertySetter<T:DisplayObject> implements PropertyAccessor<Float> implements FloatPropertyAccessor {}
 @:property("y") class DOYPropertySetter<T:DisplayObject> implements PropertyAccessor<Float> implements FloatPropertyAccessor {}
-//typedef Target2D = DisplayObject;// doesnt work on cpp {x:Float, y:Float, width:Float, height:Float};
+typedef  DisplayObject = {x:Float, y:Float, width:Float, height:Float }
