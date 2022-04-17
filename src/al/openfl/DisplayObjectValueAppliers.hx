ba5b9e8029426380c40982fcc413078b237a8cb0
artem pecheny <pecheny@gmail.com>
2022-04-18 00:12:45 +0300

    Minor fix: direct DisplayObject dependency instead of typedef.

--- src/al/openfl/DisplayObjectValueAppliers.hx
+++ src/al/openfl/DisplayObjectValueAppliers.hx
@@ -1,11 +1,11 @@
 package al.openfl;
+import openfl.display.DisplayObject;
 import al.appliers.PropertyAccessors.PropertyAccessor;
 import al.appliers.PropertyAccessors.FloatPropertyAccessor;
 
-@:property("scaleX") class DOScaleXPropertySetter<T:{scaleX:Float}> implements PropertyAccessor<Float> implements FloatPropertyAccessor {}
-@:property("scaleY") class DOScaleYPropertySetter<T:{scaleY:Float}> implements PropertyAccessor<Float> implements FloatPropertyAccessor {}
+@:property("scaleX") class DOScaleXPropertySetter<T:DisplayObject> implements PropertyAccessor<Float> implements FloatPropertyAccessor {}
+@:property("scaleY") class DOScaleYPropertySetter<T:DisplayObject> implements PropertyAccessor<Float> implements FloatPropertyAccessor {}
 @:property("width") class DOWidthPropertySetter<T:DisplayObject> implements PropertyAccessor<Float> implements FloatPropertyAccessor {}
 @:property("height") class DOHeightPropertySetter<T:DisplayObject> implements PropertyAccessor<Float> implements FloatPropertyAccessor {}
 @:property("x") class DOXPropertySetter<T:DisplayObject> implements PropertyAccessor<Float> implements FloatPropertyAccessor {}
 @:property("y") class DOYPropertySetter<T:DisplayObject> implements PropertyAccessor<Float> implements FloatPropertyAccessor {}
-typedef  DisplayObject = {x:Float, y:Float, width:Float, height:Float }
