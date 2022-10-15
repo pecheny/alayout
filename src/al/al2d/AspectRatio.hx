73168ed404f045e96d4c093fc0359d22c1857221
artem pecheny <pecheny@gmail.com>
2022-10-15 20:30:15 +0300

    Minor AspectRatio change.

--- src/al/al2d/AspectRatio.hx
+++ src/al/al2d/AspectRatio.hx
@@ -1,5 +1,5 @@
 package al.al2d;
-import haxe.ds.ReadOnlyArray;
+import Axis2D;
 
 /**
 * Assuming measure unit associated with smallest window dimension,
@@ -9,9 +9,4 @@ import haxe.ds.ReadOnlyArray;
 * Window 1500 x 1000, AspectFactors: [1.5, 1]
 * Window 1000 x 1500, AspectFactors [1, 1.5]
 **/
-abstract AspectRatio(ReadOnlyArray<Float>) from ReadOnlyArray<Float> to ReadOnlyArray<Float>{
-    public inline function getFactor(a) return this[a];
-
-    @:arrayAccess public inline function get(a:Int) return this[a];
-}
-
+typedef AspectRatio = ReadOnlyAVector2D<Float>;
