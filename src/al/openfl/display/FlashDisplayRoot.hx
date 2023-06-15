b61c5744be2dc70c0eca7ed17c8e1b5bc6010b41
artem pecheny <pecheny@gmail.com>
2023-06-15 18:57:32 +0300

    Cleanup FlashDisplayRoot.

--- src/al/openfl/display/FlashDisplayRoot.hx
+++ src/al/openfl/display/FlashDisplayRoot.hx
@@ -1,23 +1,29 @@
 package al.openfl.display;
+
 import al.ec.Entity;
 import openfl.display.Sprite;
 import openfl.display.DisplayObjectContainer;
 import ec.CtxWatcher.CtxBinder;
+
 @:keep
 class FlashDisplayRoot implements CtxBinder {
     var container:DisplayObjectContainer;
 
-    public static var instance:CtxBinder = new FlashDisplayRoot(new Sprite());
+    // public static var instance:CtxBinder = new FlashDisplayRoot(new Sprite());
     public function new(c) {
         this.container = c;
     }
 
     public function bind(e:Entity):Void {
+        trace("bind");
         var prv = e.getComponent(DrawcallDataProvider);
         if (prv == null)
             return;
-        for (v in prv.views)
+        trace("prv" + prv);
+        for (v in prv.views) {
             container.addChild(v);
+            trace(v);
+        }
     }
 
     public function unbind(e:Entity):Void {
