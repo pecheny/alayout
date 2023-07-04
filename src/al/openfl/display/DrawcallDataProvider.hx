3d0c738599f7796a9bf7aef907d0cc3face485a8
artem pecheny <pecheny@gmail.com>
2023-07-04 14:47:21 +0300

    addView() -> rebind entity.

--- src/al/openfl/display/DrawcallDataProvider.hx
+++ src/al/openfl/display/DrawcallDataProvider.hx
@@ -1,11 +1,19 @@
 package al.openfl.display;
 
+import ec.EntityHolder;
 import openfl.display.DisplayObject;
 import al.ec.Entity;
-class DrawcallDataProvider {
+
+class DrawcallDataProvider extends EntityHolder {
+    // todo make readonly
     public var views(default, null):Array<DisplayObject> = [];
 
     public function new() {
+    }
+
+    public function addView(v:DisplayObject) {
+        views.push(v);
+        entity.dispatchContext(entity);
     }
 
     public static function get(entity:Entity):DrawcallDataProvider {
