18dae5b5b84951edbdf7261464958e97ec894c52
artem pecheny <pecheny@gmail.com>
2024-02-16 08:36:47 +0300

    DataChildrenPool.

--- src/al/al2d/ChildrenPool.hx
+++ src/al/al2d/ChildrenPool.hx
@@ -7,3 +7,15 @@ import haxe.ds.ReadOnlyArray;
 import al.al2d.Widget;
 
 typedef ChildrenPool<T:IWidget<Axis2D>> = al.core.ChildrenPool<Axis2D, T>;
+
+class DataChildrenPool<TData, TButton:IWidget<Axis2D> & DataView<TData>> extends ChildrenPool<TButton> {
+    public function initData(data:Array<TData>) {
+        setActiveNum(data.length);
+        for (i in 0...data.length)
+            pool[i].initData(data[i]);
+    }
+}
+
+interface DataView<TData> {
+    function initData(descr:TData):Void;
+}
