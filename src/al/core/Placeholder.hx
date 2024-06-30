4afc3e51a972ddc7b6f3c53959858a46f1a3d103
artem pecheny <pecheny@gmail.com>
2024-06-30 23:27:48 +0300

    Placeholder.removeSibling().

--- src/al/core/Placeholder.hx
+++ src/al/core/Placeholder.hx
@@ -31,4 +31,10 @@ class PlaceholderUtils {
         for (a in ph1.axisStates.axes())
             ph1.axisStates[a].addSibling(ph2.axisStates[a]);
     }
+
+    public static function removeSibling<TAxis:Axis<TAxis>>(ph1:Placeholder<TAxis>, ph2:Placeholder<TAxis>) {
+        ph1.entity.removeChild(ph2.entity);
+        for (a in ph1.axisStates.axes())
+            ph1.axisStates[a].removeSibling(ph2.axisStates[a]);
+    }
 }
