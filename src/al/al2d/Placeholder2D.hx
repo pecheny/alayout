be5267cb415074fa4b6d69fca1a38f27229588e8
artem pecheny <pecheny@gmail.com>
2024-08-01 18:15:18 +0300

    ScreenMeasureUnit.

--- src/al/al2d/Placeholder2D.hx
+++ src/al/al2d/Placeholder2D.hx
@@ -3,4 +3,18 @@ import al.core.Placeholder;
 typedef Placeholder2D = Placeholder<Axis2D>;
 
 
+/**
+*  Options of units  to define zize in gl-like environments, when 'native' units depends on window size.
+*  @see al.al2d.AspectEatio
+**/
+@:enum abstract ScreenMeasureUnit(Axis<ScreenMeasureUnit>) to Axis<ScreenMeasureUnit> to Int {
+    /** Size inpixels */
+    var px;
+    /** Fraction of smallest screen side */
+    var sfr;
+    /** Fraction of the parent. */
+    var pfr;
+}
+
+
 
