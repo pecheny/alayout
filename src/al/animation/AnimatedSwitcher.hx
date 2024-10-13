aabd9fdda772e2a9eca0e560755386e735a217fb
artem pecheny <pecheny@gmail.com>
2024-10-13 13:36:41 +0300

    Animator and Switcher.

---
+++ src/al/animation/AnimatedSwitcher.hx
@@ -0,0 +1,61 @@
+package al.animation;
+
+import a2d.Placeholder2D;
+import al.animation.Animation.AnimationPlaceholder;
+import al.animation.AnimationTreeBuilder;
+import al.ec.WidgetSwitcher;
+import update.Updatable;
+
+class AnimatedSwitcher implements Updatable {
+    public var duration = 2.;
+    var tree:AnimationPlaceholder;
+    var time:Float = 0;
+    var switcher:WidgetSwitcher<Axis2D>;
+    var prev:Placeholder2D;
+    var prevAnim:Animator;
+    var current:Placeholder2D;
+    var curAnim:Animator;
+    var builder:AnimationTreeBuilder = new AnimationTreeBuilder();
+
+    public function new(switcher) {
+        this.switcher = switcher;
+        setTree({
+            layout: "portion",
+            children: [{size: {value: 1.}}, {size: {value: 1.}},]
+        });
+    }
+    
+    public function setTree(desc) {
+        tree = builder.build(desc);
+        tree.bindAnimation(0, t -> {
+            if (prevAnim != null)
+                prevAnim.setT(1 - t);
+        });
+        tree.bindAnimation(1, t -> {
+            if (curAnim != null)
+                curAnim.setT(t);
+        });
+    }
+
+    public function switchTo(ph:Placeholder2D) {
+        time = current != null ? 0 : 0.5;
+        prev = current;
+        prevAnim = curAnim;
+        current = ph;
+        curAnim = current.entity.getComponent(Animator);
+        switcher.bind(ph);
+    }
+
+    public function update(dt:Float):Void {
+        if (time == 1 || current == null)
+            return;
+        time += dt / duration;
+        if (time >= 1)
+            time = 1;
+        tree.setTime(time);
+        if (time == 1 && prev != null) {
+            switcher.unbind(prev);
+            prev = null;
+        }
+    }
+}
---
+++ src/al/animation/Animator.hx
@@ -0,0 +1,45 @@
+package al.animation;
+
+import al.animation.AnimationTreeBuilder;
+import al.animation.Animation;
+import ec.Component;
+import ec.Entity;
+import al.layouts.OffsetLayout;
+
+@:build(ec.macros.Macros.buildGetOrCreate())
+class Animator extends Component {
+    var tree:AnimationPlaceholder;
+    var channels:Array<Float->Void> = []; // befor init only
+    @:once var animationTreeBuilder:AnimationTreeBuilder;
+
+    override function init() {
+        tree = animationTreeBuilder.build({
+            layout: OffsetLayout.NAME,
+            children: []
+        });
+        for (ch in channels)
+            initAnim(ch);
+        channels = null;
+    }
+
+    public function setT(t:Float) {
+        if (tree == null)
+            return;
+        tree.setTime(t);
+    }
+
+    public function addAnim(h) {
+        if (_inited)
+            initAnim(h);
+        else
+            channels.push(h);
+    }
+
+    function initAnim(h) {
+        var animContainer = tree.entity.getComponent(AnimContainer);
+        var anim = animationTreeBuilder.animationWidget(new Entity(), {});
+        animationTreeBuilder.addChild(animContainer, anim);
+        anim.animations.channels.push(h);
+        animContainer.refresh();
+    }
+}
