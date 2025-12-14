ba21fc5772bb657727bd31ce3111f804af52dac9
artem pecheny <pecheny@gmail.com>
2025-12-14 18:43:36 +0300

    FlashDisplayRoot -> FlashBinder.

---
+++ src/al/openfl/display/FlashBinder.hx
@@ -0,0 +1,31 @@
+package al.openfl.display;
+
+import al.ec.Entity;
+import ec.CtxWatcher.CtxBinder;
+import openfl.display.DisplayObjectContainer;
+
+@:keep
+class FlashBinder implements CtxBinder {
+    public var container(default, null):DisplayObjectContainer;
+
+    public function new(c) {
+        this.container = c;
+    }
+
+    public function bind(e:Entity):Void {
+        var prv = e.getComponent(DrawcallDataProvider);
+        if (prv == null)
+            return;
+        for (v in prv.views) {
+            container.addChild(v);
+        }
+    }
+
+    public function unbind(e:Entity):Void {
+        var prv = e.getComponent(DrawcallDataProvider);
+        if (prv == null)
+            return;
+        for (v in prv.views)
+            container.removeChild(v);
+    }
+}
